9cc2bf58e29f052d23c0b74b3506c3250ebdd2a8
sairam2703 <137244653+sairam2703@users.noreply.github.com>
2023-06-24 13:48:35 +0300

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,24 @@
+db.photos.insertMany([
+  {
+    "title": "Sunset at the Beach",
+    "description": "A stunning sunset captured at the beach.",
+    "location": "California, USA",
+    "cameraModel": "Nikon D850",
+    "tags": ["sunset", "beach", "landscape"]
+  },
+  {
+    "title": "Portrait of a Model",
+    "description": "A captivating portrait of a professional model.",
+    "location": "Studio",
+    "cameraModel": "Canon EOS R5",
+    "tags": ["portrait", "fashion", "studio"]
+  },
+  {
+    "title": "City Lights",
+    "description": "The vibrant city lights illuminating the night.",
+    "location": "New York, USA",
+    "cameraModel": "Sony A7 III",
+    "tags": ["cityscape", "night", "urban"]
+  },
+  // ... and so on
+])
